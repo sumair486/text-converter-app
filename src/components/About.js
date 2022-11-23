a012bc69130ebddda869d6378a0380f899bea001
Sumair Shah <67826370+sumair486@users.noreply.github.com>
2022-11-23 23:26:09 +0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,1 +1,41 @@
-hi
+import React, { useState } from 'react'
+
+export default function About(props) {
+    const [MyStyle,newStyle]=useState({
+        color:'black',
+        backgroundColor:'white'
+        
+    })
+
+    const [myBtn,setBtn]=useState("Enable Dark mode")
+
+    const colorChange=()=>{
+        if(MyStyle.color==="black"){
+            newStyle({
+                color:'white',
+                backgroundColor:'black',
+                border:"1px solid white"
+            })
+            setBtn("Enable Light Mode");
+        }
+        else{
+            newStyle({
+                color:'black',
+                backgroundColor:'white'
+            })
+            setBtn("Enable Dark Modes");
+
+        }
+
+        
+    }
+
+  return (
+    
+<div className='container my-3'>
+<button onClick={colorChange} className='btn btn-primary'>{myBtn}</button>
+
+</div>
+    </div>
+  )
+}
